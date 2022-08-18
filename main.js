67afac09247ea064031b58038edeacf4c1dfff0d
dhivyameena <94811372+dhivyameena@users.noreply.github.com>
2022-08-17 22:08:43 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({
+        audio: true
+    });
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/SUTSuQQ7j/model.json', modelReady);
+}
+
+function modelReady() {
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results) {
+    console.log('got results');
+}
